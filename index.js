e3e73b114f4a7d55d7fc42297cbc4787a7cf33f9
Ilikepizza2 <lit2021010@iiitl.ac.in>
2022-12-19 11:32:59 +0530

    ported the app into node backend

--- index.js
+++ index.js
@@ -3,87 +3,188 @@ const natural = require("natural");
 const stopWords = require("stopword");
 const spell_check = require("spelling-corrector");
 const aposToLexForm = require("apos-to-lex-form");
+const express = require("express");
+const app = express();
+const port = 8080;
 
 const spellCheck = new spell_check();
 spellCheck.loadDictionary();
 
-async function getSentiment(query, url){
-  const response = async () => {
-    try{
-      await axios.get(url, {headers: {"Accept-Encoding": "gzip,deflate,compress"}});
+app.get("/api/reddit/subreddit/:query", (req, res) => {
+  const query = req.params.query;
+  const subreddit = query;
+  const url =
+    "https://social-scraper-one.vercel.app/api/reddit/subreddit/" + subreddit;
+  axios
+    .get(url, { headers: { "Accept-Encoding": "gzip,deflate,compress" } })
+    .then((response) => {
       const texts = response.data.map((post) => post.title);
       const lexedTexts = texts.map((text) => aposToLexForm(text));
       const casedTexts = lexedTexts.map((text) => text.toLowerCase());
-      const alphaOnlyTexts = casedTexts.map((text) => text.replace(/[^a-zA-Z\s]+/g, ""));
-      // console.table(alphaOnlyTexts)
+      const alphaOnlyTexts = casedTexts.map((text) =>
+        text.replace(/[^a-zA-Z\s]+/g, "")
+      );
+      
       const { WordTokenizer } = natural;
       const tokenizer = new WordTokenizer();
-      const tokenizedTexts = alphaOnlyTexts.map((text) => tokenizer.tokenize(text));
-      tokenizedTexts.map((text) => text.map((word) => spellCheck.correct(word)));
-      const noStopWordsTexts = tokenizedTexts.map((text) => stopWords.removeStopwords(text));
+      const tokenizedTexts = alphaOnlyTexts.map((text) =>
+        tokenizer.tokenize(text)
+      );
+      
+      tokenizedTexts.map((text) =>
+        text.map((word) => spellCheck.correct(word))
+      );
+      const noStopWordsTexts = tokenizedTexts.map((text) =>
+        stopWords.removeStopwords(text)
+      );
       var avgAnalysis = 0;
-      const analyzer = new natural.SentimentAnalyzer("English", natural.PorterStemmer, "afinn");
+      const analyzer = new natural.SentimentAnalyzer(
+        "English",
+        natural.PorterStemmer,
+        "afinn"
+      );
       noStopWordsTexts.forEach((text) => {
         const analysis = analyzer.getSentiment(text);
         avgAnalysis += analysis;
       });
       avgAnalysis = avgAnalysis / noStopWordsTexts.length;
-      // console.table(noStopWordsTexts)
-      return {status: "200", analysis: avgAnalysis};
-    }
-    catch(error){
-      return {status: "404", analysis: "Not Found"};
-    }
-  }
-}
+      res.send({ status: "200", analysis: avgAnalysis });
+    })
+    .catch((error) => {
+      res.send({ status: "404", analysis: "Not Found" });
+    });
+});
 
-async function getSentimentSubreddit(query) {
-  if (query.includes(" ")) {
-    query = query.replace(" ", "+");
-  }
-  const subreddit = query;
-  const url =
-    "https://social-scraper-one.vercel.app/api/reddit/subreddit/" + subreddit;
-  const response = await getSentiment(query, url);
-  return response;
-}
-
-async function getSentimentRedditSearch(query) {
-  if (query.includes(" ")) {
-    query = query.replace(" ", "+");
-  }
+app.get("/api/reddit/search/:query", (req, res) => {
+  const query = req.params.query;
   const tags = query;
   const url = "https://social-scraper-one.vercel.app/api/reddit/tags/" + tags;
-  const response = await getSentiment(query, url);
-  return response;
-}
+  axios
+    .get(url, { headers: { "Accept-Encoding": "gzip,deflate,compress" } })
+    .then((response) => {
+      const texts = response.data.map((post) => post.title);
+      const lexedTexts = texts.map((text) => aposToLexForm(text));
+      const casedTexts = lexedTexts.map((text) => text.toLowerCase());
+      const alphaOnlyTexts = casedTexts.map((text) =>
+        text.replace(/[^a-zA-Z\s]+/g, "")
+      );
+      
+      const { WordTokenizer } = natural;
+      const tokenizer = new WordTokenizer();
+      const tokenizedTexts = alphaOnlyTexts.map((text) =>
+        tokenizer.tokenize(text)
+      );
+      
+      tokenizedTexts.map((text) =>
+        text.map((word) => spellCheck.correct(word))
+      );
+      const noStopWordsTexts = tokenizedTexts.map((text) =>
+        stopWords.removeStopwords(text)
+      );
+      var avgAnalysis = 0;
+      const analyzer = new natural.SentimentAnalyzer(
+        "English",
+        natural.PorterStemmer,
+        "afinn"
+      );
+      noStopWordsTexts.forEach((text) => {
+        const analysis = analyzer.getSentiment(text);
+        avgAnalysis += analysis;
+      });
+      avgAnalysis = avgAnalysis / noStopWordsTexts.length;
+      res.send({ status: "200", analysis: avgAnalysis });
+    })
+    .catch((error) => {
+      res.send({ status: "404", analysis: "Not Found" });
+    });
+});
 
-async function getSentimentStackoverflow(query) {
-  if (query.includes(" ")) {
-    query = query.replace(" ", "+");
-  }
-  const tags = query;
+app.get("/api/stackoverflow/:query", async (req, res) => {
+  const query = req.params.query;
   const url =
-    "https://social-scraper-one.vercel.app/api/stackoverflow/tags/" + tags;
-  const response = await getSentiment(query, url);
-  return response;
-}
+    "https://social-scraper-one.vercel.app/api/stackoverflow/tags/" + query;
+  axios
+    .get(url, { headers: { "Accept-Encoding": "gzip,deflate,compress" } })
+    .then((response) => {
+      const texts = response.data.map((post) => post.title);
+      const lexedTexts = texts.map((text) => aposToLexForm(text));
+      const casedTexts = lexedTexts.map((text) => text.toLowerCase());
+      const alphaOnlyTexts = casedTexts.map((text) =>
+        text.replace(/[^a-zA-Z\s]+/g, "")
+      );
+      
+      const { WordTokenizer } = natural;
+      const tokenizer = new WordTokenizer();
+      const tokenizedTexts = alphaOnlyTexts.map((text) =>
+        tokenizer.tokenize(text)
+      );
+      
+      tokenizedTexts.map((text) =>
+        text.map((word) => spellCheck.correct(word))
+      );
+      const noStopWordsTexts = tokenizedTexts.map((text) =>
+        stopWords.removeStopwords(text)
+      );
+      var avgAnalysis = 0;
+      const analyzer = new natural.SentimentAnalyzer(
+        "English",
+        natural.PorterStemmer,
+        "afinn"
+      );
+      noStopWordsTexts.forEach((text) => {
+        const analysis = analyzer.getSentiment(text);
+        avgAnalysis += analysis;
+      });
+      avgAnalysis = avgAnalysis / noStopWordsTexts.length;
+      res.send({ status: "200", analysis: avgAnalysis });
+    });
+});
 
-async function getSentimentDuckduckgo(query) {
-  if (query.includes(" ")) {
-    query = query.replace(" ", "+");
-  }
-  console.log(query);
-  const tags = query;
-  const url = "https://social-scraper-one.vercel.app/api/duckduckgo/" + tags;
-  const response = await getSentiment(query, url);
-  return response;
-}
+app.get("/api/duckduckgo/:query", async (req, res) => {
+  const query = req.params.query;
+  const url =
+    "https://social-scraper-one.vercel.app/api/duckduckgo/tags/" + query;
+  axios
+    .get(url, { headers: { "Accept-Encoding": "gzip,deflate,compress" } })
+    .then((response) => {
+      const texts = response.data.map((post) => post.title);
+      const lexedTexts = texts.map((text) => aposToLexForm(text));
+      const casedTexts = lexedTexts.map((text) => text.toLowerCase());
+      const alphaOnlyTexts = casedTexts.map((text) =>
+        text.replace(/[^a-zA-Z\s]+/g, "")
+      );
+      
+      const { WordTokenizer } = natural;
+      const tokenizer = new WordTokenizer();
+      const tokenizedTexts = alphaOnlyTexts.map((text) =>
+        tokenizer.tokenize(text)
+      );
+      
+      tokenizedTexts.map((text) =>
+        text.map((word) => spellCheck.correct(word))
+      );
+      const noStopWordsTexts = tokenizedTexts.map((text) =>
+        stopWords.removeStopwords(text)
+      );
+      var avgAnalysis = 0;
+      const analyzer = new natural.SentimentAnalyzer(
+        "English",
+        natural.PorterStemmer,
+        "afinn"
+      );
+      noStopWordsTexts.forEach((text) => {
+        const analysis = analyzer.getSentiment(text);
+        avgAnalysis += analysis;
+      });
+      avgAnalysis = avgAnalysis / noStopWordsTexts.length;
+      res.send({ status: "200", analysis: avgAnalysis });
+    })
+    .catch((error) => {
+      res.send({ status: "404", analysis: "Not Found" });
+    });
+});
 
-module.exports = {
-  getSentiment,
-  getSentimentRedditSearch,
-  getSentimentSubreddit,
-  getSentimentStackoverflow,
-  getSentimentDuckduckgo,
-};
+// listen at port
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
